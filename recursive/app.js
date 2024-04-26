fbe084c9a35001e16eea7e6f66f7fea26b7d2075
unknown <jamilafzal255@gmail.com>
2024-04-26 10:09:55 +0500

    recursion

---
+++ recursive/app.js
@@ -0,0 +1,39 @@
+// let count =1
+// const demo = (num) => {
+
+//     if (count>num) {
+//         return
+//     }
+//     console.log(count);
+//     count++
+//     demo(num)
+// }
+// demo(10)
+
+
+// function factorial(n) {
+//     // Base case: factorial of 0 is 1
+//     if (n === 0) {
+//         return 1;
+//     }
+//     // Recursive case: factorial of n is n multiplied by factorial of (n - 1)
+//     return  n * factorial(n - 1);
+// }
+
+// // Test the factorial function
+// const num = 5; // Change the number to calculate its factorial
+// console.log(factorial(num));
+
+
+// simplfy
+
+// const factorial = (num) => {
+//     if (num === 0) {
+//         return 1
+//     }
+
+//     return num * factorial(num - 1)
+// }
+// const num = 4
+// const result = factorial(num);
+// console.log(result);
---
+++ recursive/fact.js
@@ -0,0 +1,10 @@
+// const fact = (num) => {
+//     if (num === 0) {
+//         return 1
+//     }
+//     return num * fact(num - 1)
+// }
+// const num = 6
+// console.log(fact(num))
+
+
